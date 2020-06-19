2fc1960d4b930e8b7cb9b40efd2380b459b3abc2
liuhao326 <326047736@qq.com>
2020-06-20 03:15:33 +0800

    支持全屏图、left="0px"小图及注释的复制

--- wereader/inject-copy.js
+++ wereader/inject-copy.js
@@ -8,33 +8,30 @@ function setMesToBg(picStr){
     });
 }
 
-//遍历HTMLCollection
+//遍历HTMLCollection生成按钮
 function imgshandle(imgs,count){
+    console.log("imgshandle(imgs,count)被调用")
     for(var i=0,len=imgs.length;i<len;i++){
-        if(imgs[i].className == "width90 wr_absolute wr_readerImage_opacity wr_pendingLoading"){
-            alert("为了得到准确的图片，请滚动页面确保所有图片都已加载完毕~");
-            return
-        }
-    }
-    for(count,C=count,len=count + imgs.length;count<len;count++){
-        /* if(imgs[i].className.substr(0,5) != "width"){
-            console.log("特殊className")
-            continue
-        } */
-        var src = imgs[count-C].src
+        count = count + i
+        var src = imgs[i].src
         let picStr = "![" + src.split("/").pop() + "](" + src + ")"
-        var top = imgs[count-C].style.top
+        var top = imgs[i].style.top
         var btn =  document.createElement("a" + count);
         btn.innerHTML = "📋";
         btn.id = "linkCopy" + count
         btn.className = "wr_absolute wr_readerImage_opacity"
-        //btn.style.cssText = "border-radius: 3px;background-color: rgb(227,227,227);"
-        btn.style.top = parseInt(top.substr(0, top.length - 2)) - 20 + "px"
-        btn.style.right = "0px"
+        //判断是否为style.left == "0px"的小图
+        if(imgs[i].style.left == "0px"){
+            btn.style.left = "0px"
+            btn.style.top = parseInt(top.substr(0, top.length - 2)) - 30 + "px"
+        }else{
+            btn.style.right = "0px"
+            btn.style.top = parseInt(top.substr(0, top.length - 2)) - 20 + "px"
+        }
         btn.style.width = "16px"
         btn.style.cursor = "pointer"
-        var parent = imgs[count-C].parentNode
-        var inser = parent.insertBefore(btn,imgs[count-C]);
+        var parent = imgs[i].parentNode
+        var inser = parent.insertBefore(btn,imgs[i]);
         inser.addEventListener('click', function(){
             this.innerHTML = "✔"
             setMesToBg(picStr)
@@ -44,17 +41,91 @@ function imgshandle(imgs,count){
             }, 1000);
          }, false);
     }
-    return count
+    return count + 1
+}
+
+//遍历HTMLCollection检查图片是否加载完毕
+function isOver(imgs){
+    console.log("isOver(imgs)被调用")
+    for(var i=0,len=imgs.length;i<len;i++){
+        if(imgs[i].className.indexOf("wr_pendingLoading") != -1){
+            return false
+        }
+    }
+    return true
+}
+
+//给注释添加复制功能
+function addCopyBtn2(){
+    console.log("addCopyBtn2()被调用")
+    var footerNotes = document.getElementsByClassName("reader_footer_note js_readerFooterNote wr_absolute");
+    //遍历注释控件
+    for(var i=0,len=footerNotes.length;i<len;i++){
+        var footernote = footerNotes[i].getAttribute("data-wr-footernote")
+        var top = footerNotes[i].style.top
+        var left = footerNotes[i].style.left
+        var btn =  document.createElement("n" + i);
+        btn.style.left = parseInt(left.substr(0, left.length - 2)) + 60 + "px"
+        btn.style.top = parseInt(top.substr(0, top.length - 2)) - 6 + "px"
+        btn.style.width = "16px"
+        btn.style.cursor = "pointer"
+        btn.style.display = "none"
+        btn.innerHTML = "📋";
+        btn.id = "noteCopy" + i
+        btn.className = "wr_absolute wr_readerImage_opacity"
+        var parent = footerNotes[i].parentNode
+        var inser = parent.insertBefore(btn,footerNotes[i]);
+        //给复制按钮注册点击事件
+        inser.addEventListener('click', function(){
+            this.innerHTML = "✔"
+            setMesToBg(footernote)
+            var id = this.id
+            //控件被点击后一秒消失
+            setTimeout(function () {
+                document.getElementById(id).style.display = "none"
+                document.getElementById(id).innerHTML = "📋"
+            }, 1000);
+         }, false);
+         //给注释按钮注册点击事件：点击后复制按钮变为可见
+         footerNotes[i].addEventListener('click', function(){
+            btn.style.display = "block"
+        },false)
+    }
 }
 
 //添加复制按钮
-function addCopyBtn(){
-    console.log("addCopyBtn()被调用");
+function addCopyBtn1(){
+    console.log("addCopyBtn1()被调用");
     var imgs1 = document.getElementsByClassName("width90 wr_absolute wr_readerImage_opacity");
     var imgs2 = document.getElementsByClassName("width80 wr_absolute wr_readerImage_opacity");
+    var imgs3 = document.getElementsByClassName("qqreader-fullimg wr_absolute wr_readerImage_opacity");
+    var imgs4 = document.getElementsByClassName("h-pic wr_absolute wr_readerImage_opacity");
+    if(isOver(imgs1) == false||isOver(imgs2) == false||isOver(imgs3) == false||isOver(imgs4) == false){
+        alert("为了得到准确的图片，请滚动页面确保所有图片都已加载完毕~");
+        return
+    }
     var count = imgshandle(imgs1,0);
-    imgshandle(imgs2,count);
+    count = imgshandle(imgs2,count);
+    count = imgshandle(imgs3,count);
+    count = imgshandle(imgs4,count);
 }
 
 console.log("inject-copy.js已注入")
-addCopyBtn();
+addCopyBtn1();
+addCopyBtn2()
+//给body注册点击事件，用于定时隐藏注册复制按钮
+document.body.addEventListener('click', function(){
+    console.log("document.body.addEventListener()监听到消息并进入了回调函数")
+    setTimeout(function () {
+        if(document.getElementsByClassName("reader_footerNote_mask").length == 0){
+            var nodes = document.getElementsByClassName("wr_absolute wr_readerImage_opacity")
+            for(var i=0,len=nodes.length;i<len;i++){
+                if(nodes[i].id.substr(0,8) == "noteCopy"){
+                    if(nodes[i].innerHTML != "✔"){
+                        nodes[i].style.display = "none"
+                    }
+                }
+            }
+        }
+    }, 2500);
+},false)
