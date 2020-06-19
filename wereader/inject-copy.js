d57b0e6e46a71e5494c4579b72a8789c455e2270
liuhao326 <326047736@qq.com>
2020-06-19 23:46:14 +0800

    支持width80类图片

--- wereader/inject-copy.js
+++ wereader/inject-copy.js
@@ -8,36 +8,33 @@ function setMesToBg(picStr){
     });
 }
 
-//添加复制按钮
-function addCopyBtn(){
-    console.log("addCopyBtn()被调用");
-    var imgs = document.getElementsByClassName("width90 wr_absolute wr_readerImage_opacity");
-    console.log("imgs.length:\n" + imgs.length);
+//遍历HTMLCollection
+function imgshandle(imgs,count){
     for(var i=0,len=imgs.length;i<len;i++){
         if(imgs[i].className == "width90 wr_absolute wr_readerImage_opacity wr_pendingLoading"){
             alert("为了得到准确的图片，请滚动页面确保所有图片都已加载完毕~");
             return
         }
     }
-    for(var i=0,len=imgs.length;i<len;i++){
+    for(count,C=count,len=count + imgs.length;count<len;count++){
         /* if(imgs[i].className.substr(0,5) != "width"){
             console.log("特殊className")
             continue
         } */
-        var src = imgs[i].src
+        var src = imgs[count-C].src
         let picStr = "![" + src.split("/").pop() + "](" + src + ")"
-        var top = imgs[i].style.top
-        var btn =  document.createElement("a" + i);
+        var top = imgs[count-C].style.top
+        var btn =  document.createElement("a" + count);
         btn.innerHTML = "📋";
-        btn.id = "linkCopy" + i
+        btn.id = "linkCopy" + count
         btn.className = "wr_absolute wr_readerImage_opacity"
         //btn.style.cssText = "border-radius: 3px;background-color: rgb(227,227,227);"
         btn.style.top = parseInt(top.substr(0, top.length - 2)) - 20 + "px"
         btn.style.right = "0px"
         btn.style.width = "16px"
         btn.style.cursor = "pointer"
-        var parent = imgs[i].parentNode
-        var inser = parent.insertBefore(btn,imgs[i]);
+        var parent = imgs[count-C].parentNode
+        var inser = parent.insertBefore(btn,imgs[count-C]);
         inser.addEventListener('click', function(){
             this.innerHTML = "✔"
             setMesToBg(picStr)
@@ -47,7 +44,16 @@ function addCopyBtn(){
             }, 1000);
          }, false);
     }
-    
+    return count
+}
+
+//添加复制按钮
+function addCopyBtn(){
+    console.log("addCopyBtn()被调用");
+    var imgs1 = document.getElementsByClassName("width90 wr_absolute wr_readerImage_opacity");
+    var imgs2 = document.getElementsByClassName("width80 wr_absolute wr_readerImage_opacity");
+    var count = imgshandle(imgs1,0);
+    imgshandle(imgs2,count);
 }
 
 console.log("inject-copy.js已注入")
