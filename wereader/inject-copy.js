81b89679b31aecca61a615c9a523287a2fcec2d6
liuhao326 <326047736@qq.com>
2020-06-20 15:31:59 +0800

    精简

--- wereader/inject-copy.js
+++ wereader/inject-copy.js
@@ -1,24 +1,22 @@
-
 //发送消息给后台复制内容
-function setMesToBg(picStr){
-    console.log("setMesToBg(picStr)被调用，参数picStr：\n" + picStr)
-    console.log("setMesToBg(picStr)开始传递信息给后台")
+function sendMsgToBg(picStr){
+    console.log("sendMsgToBg(picStr)：被调用，参数picStr：\n" + picStr)
+    console.log("sendMsgToBg(picStr)开始传递信息给后台")
     chrome.runtime.sendMessage({picText: picStr}, function(response) {
-        console.log('setMesToBg(picStr)收到来自后台的回复：' + response);
+        console.log('sendMsgToBg(picStr)收到来自后台的回复：' + response);
     });
 }
 
-//遍历HTMLCollection生成按钮
-function imgshandle(imgs,count){
-    console.log("imgshandle(imgs,count)被调用")
+//为图片遍历HTMLCollection生成按钮
+function generateBtn(imgs){
+    console.log("generateBtn(imgs)：被调用")
     for(var i=0,len=imgs.length;i<len;i++){
-        count = count + i
         var src = imgs[i].src
         let picStr = "![" + src.split("/").pop() + "](" + src + ")"
         var top = imgs[i].style.top
-        var btn =  document.createElement("a" + count);
+        var btn =  document.createElement("a" + i);
         btn.innerHTML = "📋";
-        btn.id = "linkCopy" + count
+        btn.id = "linkCopy" + i
         btn.className = "wr_absolute wr_readerImage_opacity"
         //判断是否为style.left == "0px"的小图
         if(imgs[i].style.left == "0px"){
@@ -34,19 +32,18 @@ function imgshandle(imgs,count){
         var inser = parent.insertBefore(btn,imgs[i]);
         inser.addEventListener('click', function(){
             this.innerHTML = "✔"
-            setMesToBg(picStr)
+            sendMsgToBg(picStr)
             var id = this.id
             setTimeout(function () {
                 document.getElementById(id).innerHTML = "📋"
             }, 1000);
          }, false);
     }
-    return count + 1
 }
 
 //遍历HTMLCollection检查图片是否加载完毕
 function isOver(imgs){
-    console.log("isOver(imgs)被调用")
+    console.log("isOver(imgs)：被调用")
     for(var i=0,len=imgs.length;i<len;i++){
         if(imgs[i].className.indexOf("wr_pendingLoading") != -1){
             return false
@@ -57,7 +54,7 @@ function isOver(imgs){
 
 //给注释添加复制功能
 function addCopyBtn2(){
-    console.log("addCopyBtn2()被调用")
+    console.log("addCopyBtn2()：被调用")
     var footerNotes = document.getElementsByClassName("reader_footer_note js_readerFooterNote wr_absolute");
     //遍历注释控件
     for(var i=0,len=footerNotes.length;i<len;i++){
@@ -78,7 +75,7 @@ function addCopyBtn2(){
         //给复制按钮注册点击事件
         inser.addEventListener('click', function(){
             this.innerHTML = "✔"
-            setMesToBg(footernote)
+            sendMsgToBg(footernote)
             var id = this.id
             //控件被点击后一秒消失
             setTimeout(function () {
@@ -95,27 +92,21 @@ function addCopyBtn2(){
 
 //添加复制按钮
 function addCopyBtn1(){
-    console.log("addCopyBtn1()被调用");
-    var imgs1 = document.getElementsByClassName("width90 wr_absolute wr_readerImage_opacity");
-    var imgs2 = document.getElementsByClassName("width80 wr_absolute wr_readerImage_opacity");
-    var imgs3 = document.getElementsByClassName("qqreader-fullimg wr_absolute wr_readerImage_opacity");
-    var imgs4 = document.getElementsByClassName("h-pic wr_absolute wr_readerImage_opacity");
-    if(isOver(imgs1) == false||isOver(imgs2) == false||isOver(imgs3) == false||isOver(imgs4) == false){
+    console.log("addCopyBtn1()：被调用");
+    var imgs = document.getElementById("renderTargetContent").getElementsByTagName("img");
+    if(isOver(imgs) == false){
         alert("为了得到准确的图片，请滚动页面确保所有图片都已加载完毕~");
         return
     }
-    var count = imgshandle(imgs1,0);
-    count = imgshandle(imgs2,count);
-    count = imgshandle(imgs3,count);
-    count = imgshandle(imgs4,count);
+    generateBtn(imgs);
 }
 
-console.log("inject-copy.js已注入")
+console.log("inject-copy.js：\ninject-copy.js已注入")
 addCopyBtn1();
 addCopyBtn2()
 //给body注册点击事件，用于定时隐藏注册复制按钮
 document.body.addEventListener('click', function(){
-    console.log("document.body.addEventListener()监听到消息并进入了回调函数")
+    console.log("inject-copy.js:\ndocument.body.addEventListener()监听到消息开始计时")
     setTimeout(function () {
         if(document.getElementsByClassName("reader_footerNote_mask").length == 0){
             var nodes = document.getElementsByClassName("wr_absolute wr_readerImage_opacity")
