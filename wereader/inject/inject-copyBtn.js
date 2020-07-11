0275f7312b0d0c430fed435f9800a9835302bf72
liuhao326 <326047736@qq.com>
2020-07-11 18:41:26 +0800

    improve copyBtn for notes

--- wereader/inject/inject-copyBtn.js
+++ wereader/inject/inject-copyBtn.js
@@ -56,11 +56,54 @@ function addCopyBtn1(){
     generateBtn(imgs);
 }
 
-console.log("inject-copyBtn.js：\ninject-copyBtn.js已注入")
-if(document.getElementById("linkCopy0") == undefined){
-    addCopyBtn1();
+function addCopyBtn2(){
+    var footerNotes = document.getElementsByClassName("reader_footer_note js_readerFooterNote wr_absolute");
+    //遍历注释控件
+    for(var i=0,len=footerNotes.length;i<len;i++){
+        console.log("for => i = " + i)
+        //获取注释内容、注释按钮位置等信息
+        let footernote = footerNotes[i].getAttribute("data-wr-footernote")
+        let btn =  document.createElement("a0")
+        btn.style.width = "19px"
+        btn.style.height = "19px"
+        btn.style.cursor = "pointer"
+        btn.style.display = "block"
+        btn.style.fontSize = "19px"
+        btn.innerHTML = "📋"
+        btn.id = "noteCopy" + i
+        btn.addEventListener('click', function(){
+            sendMsgToBg(footernote)
+            this.innerHTML = "✔"
+            let id = this.id
+            //控件被点击后一秒
+            setTimeout(function () {
+                document.getElementById(id).innerHTML = "📋"
+            }, 1000);
+        }, false);
+        //btn.className = "wr_absolute wr_readerImage_opacity"
+        //给注释按钮注册点击事件
+        footerNotes[i].addEventListener('click', function(){
+            var interval = setInterval(() => {
+                let p = document.getElementsByClassName("reader_footerNote_text")[0]
+                if(p != undefined){
+                    console.log("setInterval() => p != undefined")
+                    let parent = p.parentNode
+                    parent.appendChild(btn)
+                    //结束定时器
+                    clearInterval(interval)
+                }else{
+                    console.log("setInterval() => p == undefined，重新获取")
+                }
+            },10)
+        },false)
+    }
 }
 
+console.log("inject-copyBtn.js：\ninject-copyBtn.js已注入")
+if(document.getElementById("linkCopy0") == undefined){
+    addCopyBtn1()
+    addCopyBtn2()
+}
 //给注释添加复制功能
 /*
 function addCopyBtn2(){
