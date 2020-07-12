186274565d420a00c27f4471f265b500dec52db9
liuhao326 <326047736@qq.com>
2020-07-12 19:38:09 +0800

    完善：注释复制按钮

--- wereader/inject/inject-copyBtn.js
+++ wereader/inject/inject-copyBtn.js
@@ -73,20 +73,19 @@ function addCopyBtn2(){
         btn.id = "noteCopy" + i
         btn.addEventListener('click', function(){
             sendMsgToBg(footernote)
-            this.innerHTML = "✔"
-            let id = this.id
-            //控件被点击后一秒
-            setTimeout(function () {
-                document.getElementById(id).innerHTML = "📋"
-            }, 1000);
         }, false);
+        btn.onmousedown = function(){
+            this.innerHTML = "✔"
+        }
+        btn.onmouseup = function(){
+            this.innerHTML = "📋"
+        }
         //btn.className = "wr_absolute wr_readerImage_opacity"
         //给注释按钮注册点击事件
         footerNotes[i].addEventListener('click', function(){
             var interval = setInterval(() => {
                 let p = document.getElementsByClassName("reader_footerNote_text")[0]
                 if(p != undefined){
-                    console.log("setInterval() => p != undefined")
                     let parent = p.parentNode
                     parent.appendChild(btn)
                     //结束定时器
@@ -104,77 +103,3 @@ if(document.getElementById("linkCopy0") == undefined){
     addCopyBtn1()
     addCopyBtn2()
 }
-//给注释添加复制功能
-/*
-function addCopyBtn2(){
-    console.log("addCopyBtn2()：被调用")
-    var footerNotes = document.getElementsByClassName("reader_footer_note js_readerFooterNote wr_absolute");
-    //遍历注释控件
-    for(var i=0,len=footerNotes.length;i<len;i++){
-        //获取注释内容、注释按钮位置等信息
-        let footernote = footerNotes[i].getAttribute("data-wr-footernote")
-        let top = footerNotes[i].style.top
-        let left = footerNotes[i].style.left
-        //创建复制按钮并嵌入到注释控件位置之前
-        let btn =  document.createElement("n" + i);
-        btn.style.left = parseInt(left.substr(0, left.length - 2)) + 60 + "px"
-        btn.style.top = parseInt(top.substr(0, top.length - 2)) - 6 + "px"
-        btn.style.width = "16px"
-        btn.style.cursor = "pointer"
-        btn.style.display = "none"
-        btn.innerHTML = "📋";
-        btn.id = "noteCopy" + i
-        btn.className = "wr_absolute wr_readerImage_opacity"
-        let parent = footerNotes[i].parentNode
-        let inser = parent.insertBefore(btn,footerNotes[i])
-        //给复制按钮注册点击事件
-        inser.addEventListener('click', function(){
-            this.innerHTML = "✔"
-            sendMsgToBg(footernote)
-            let id = this.id
-            //控件被点击后一秒消失
-            setTimeout(function () {
-                document.getElementById(id).style.display = "none"
-                document.getElementById(id).innerHTML = "📋"
-            }, 1000);
-         }, false);
-         //给注释按钮注册点击事件：点击后复制按钮变为可见
-         footerNotes[i].addEventListener('click', function(){
-            btn.style.display = "block"
-        },false)
-    }
-}
-*/
-
-//注释复制按钮直接content.js生成，故舍弃不用
-/* if(document.getElementById("noteCopy0") == undefined){
-    addCopyBtn2()
-    //给body注册点击事件，用于定时隐藏注释复制按钮
-    document.body.addEventListener('click', function(){
-        console.log("inject-copyBtn.js:\ndocument.body.addEventListener()监听到消息开始计时")
-        setTimeout(function () {
-            if(document.getElementsByClassName("reader_footerNote_mask").length == 0){
-                var nodes = document.getElementsByClassName("wr_absolute wr_readerImage_opacity")
-                for(var i=0,len=nodes.length;i<len;i++){
-                    if(nodes[i].id.substr(0,8) == "noteCopy"){
-                        if(nodes[i].innerHTML != "✔"){
-                            nodes[i].style.display = "none"
-                        }
-                    }
-                }
-            }
-        }, 2500);
-    },false)
-} */
-
-//遍历HTMLCollection检查图片是否加载完毕
-/*舍弃*/
-/* function isOver(imgs){
-    console.log("isOver(imgs)：被调用")
-    for(var i=0,len=imgs.length;i<len;i++){
-        if(imgs[i].className.indexOf("wr_pendingLoading") != -1){
-            return false
-        }
-    }
-    return true
-} */
