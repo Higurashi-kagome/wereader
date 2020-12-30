a0fb847595a7868f096f0dc55edc5b6c0c8d459e
Liuhao <326047736@qq.com>
2020-12-30 21:13:49 +0800

    fix bug of img export; support export footnote and inline img export

--- wereader-chrome/inject/inject-copyImgs.js
+++ wereader-chrome/inject/inject-copyImgs.js
@@ -6,55 +6,62 @@ function setMesToBg(imgsArray){
 }
 
 //获取imgs数组
-function requestImgsArray(imgs,s0,s1,s2){
-    var imgsArray = []
-    //遍历imgs元素
-    for(var i=0,len=imgs.length;i<len;i=i+1){
-        var s = imgs[i].getAttribute("data-src")
-        if(s == null || s == ""){
-            Swal.fire({title: "Oops...",html: "requestImgsArray()：<br>图片链接获取失败。",icon: "error",confirmButtonText: '确定'})
-            return
-        }
-        var l = imgs[i].style.left
-        var t = imgs[i].style.top
-        var w = imgs[i].style.width
-        var h = imgs[i].style.height
-        var leftImg = parseFloat(l.substr(0, l.length - 2))
-        var topImg = parseFloat(t.substr(0, t.length - 2))
-        var widthImg = parseFloat(w.substr(0, w.length - 2))
-        var heightImg = parseFloat(h.substr(0, h.length - 2))
-        //遍历标注元素更新 imgArray，根据标注元素与图片在网站中的位置来判断图片是否被标注
-        function ergodic(markElements){
-            for(var j=0,len2=markElements.length;j<len2;j++){
-                var leftDiv = parseFloat(markElements[j].style.left.substr(0, markElements[j].style.left.length - 2))
-                var topDiv = parseFloat(markElements[j].style.top.substr(0, markElements[j].style.top.length - 2))
-                var widthDiv = parseFloat(markElements[j].style.width.substr(0, markElements[j].style.width.length - 2))
-                var heightDiv = parseFloat(markElements[j].style.height.substr(0, markElements[j].style.height.length - 2))
-                if(Math.abs(leftImg - leftDiv)<=0.1 && Math.abs(topImg - topDiv)<=0.1 && Math.abs(widthImg - widthDiv)<=0.1 && Math.abs(heightImg - heightDiv)<=0.1){
-                    imgsArray.push([s.split("/").pop(),s])
-                    return 1
+function requestImgsArray(imgAndnoteTags,s0,s1,s2){
+    let imgsArray = []
+    let notesCounter = 0
+    var top = 0, height = 0
+    var imgSrc = '', footnote = ''
+    var currentContent = ''
+    if(document.getElementsByClassName("readerTopBar_title_chapter")[0]){
+        currentContent = document.getElementsByClassName("readerTopBar_title_chapter")[0].textContent
+    }else{
+        currentContent = document.getElementsByClassName("chapterItem chapterItem_current")[0].childNodes[0].childNodes[0].textContent
+    }
+    //遍历标注元素更新 imgArray，根据标注元素与图片/注释在网站中的位置来判断图片/注释是否被标注
+    function ergodic(markMask, isImg){
+        for(let j=0,len2=markMask.length;j<len2;j++){
+            let topDiv = parseFloat(markMask[j].style.top.replace('px', ''))
+            let heightDiv = parseFloat(markMask[j].style.height.replace('px', ''))
+            console.log(top + height - topDiv - heightDiv)
+            if(Math.abs(top + height - topDiv - heightDiv)<=0.1){
+                if(isImg){
+                    imgsArray.push({alt: imgSrc.split("/").pop(), src: imgSrc})
+                }else{
+                    imgsArray.push({name: `${currentContent}-${notesCounter}`, footnote: footnote})
+                    notesCounter++
                 }
+                return 1
             }
-            return 0
+        }
+        return 0
+    }
+    for(let i=0,len=imgAndnoteTags.length;i<len;i++){
+        imgSrc = imgAndnoteTags[i].getAttribute("data-src")
+        footnote = imgAndnoteTags[i].getAttribute("data-wr-footernote")
+        height = parseFloat(imgAndnoteTags[i].style.height.replace('px', ''))
+        //注释图标的位置需要修正
+        if(imgSrc){
+            top = parseFloat(imgAndnoteTags[i].style.top.replace('px', ''))
+        }else{
+            top = parseFloat(imgAndnoteTags[i].style.top.replace('px', '')) + 2.8
         }
         //遍历各级别标注
-        if(!ergodic(s0)){
-            if(!ergodic(s1)){
-                ergodic(s2)
-            }
-        }
+        if(!ergodic(s0, imgSrc))
+            if(!ergodic(s1, imgSrc))
+                ergodic(s2, imgSrc)
     }
+    console.log(imgsArray)
     return imgsArray
 }
 
 //入口
 function main(){
     //获取正文所有图片
-    var imgs = document.getElementById("renderTargetContent").getElementsByTagName("img");
+    var imgAndnoteTags = document.querySelectorAll(".wr_readerImage_opacity,.reader_footer_note.js_readerFooterNote.wr_absolute");
     var s0 = document.getElementsByClassName("wr_underline s0");
     var s1 = document.getElementsByClassName("wr_underline s1");
     var s2 = document.getElementsByClassName("wr_underline s2");
-    var imgsArray = requestImgsArray(imgs,s0,s1,s2);
+    var imgsArray = requestImgsArray(imgAndnoteTags,s0,s1,s2);
     setMesToBg(imgsArray)
 }
 
