61325835c4a4e33c92c45d4de9e4852484319c8f
Liuhao <326047736@qq.com>
2020-12-31 16:01:25 +0800

    improve: outline img add LF

--- wereader-chrome/inject/inject-copyImgs.js
+++ wereader-chrome/inject/inject-copyImgs.js
@@ -1,15 +1,16 @@
 /*用于获取被标注的图片的Markdown文本数组，用于支持导出被标注的图片*/
 
 //发送消息给后台
-function setMesToBg(imgsArray){
-    chrome.runtime.sendMessage({type: "imgsArr", RimgsArr: imgsArray});
+function setMesToBg(imgsAndNotes){
+    chrome.runtime.sendMessage({type: "imgsAndNotes", imgsAndNotes: imgsAndNotes});
 }
 
-//获取imgs数组
-function requestImgsArray(imgAndnoteTags,s0,s1,s2){
-    let imgsArray = []
+//获取图片和注释
+function requestImgNotes(imgAndnoteTags,s0,s1,s2){
+    let imgsAndNotes = []
     let notesCounter = 0
     var top = 0, height = 0
+    var isInlineImg = false
     var imgSrc = '', footnote = ''
     var currentContent = ''
     if(document.getElementsByClassName("readerTopBar_title_chapter")[0]){
@@ -17,17 +18,17 @@ function requestImgsArray(imgAndnoteTags,s0,s1,s2){
     }else{
         currentContent = document.getElementsByClassName("chapterItem chapterItem_current")[0].childNodes[0].childNodes[0].textContent
     }
-    //遍历标注元素更新 imgArray，根据标注元素与图片/注释在网站中的位置来判断图片/注释是否被标注
-    function ergodic(markMask, isImg){
+    //遍历标注元素更新 imgsAndNotes，根据标注元素与图片/注释在网站中的位置来判断图片/注释是否被标注
+    function ergodic(markMask){
         for(let j=0,len2=markMask.length;j<len2;j++){
-            let topDiv = parseFloat(markMask[j].style.top.replace('px', ''))
-            let heightDiv = parseFloat(markMask[j].style.height.replace('px', ''))
-            console.log(top + height - topDiv - heightDiv)
-            if(Math.abs(top + height - topDiv - heightDiv)<=0.1){
-                if(isImg){
-                    imgsArray.push({alt: imgSrc.split("/").pop(), src: imgSrc})
+            let divTop = parseFloat(markMask[j].style.top.replace('px', ''))
+            let divHeight = parseFloat(markMask[j].style.height.replace('px', ''))
+            console.log(top + height - divTop - divHeight)
+            if(Math.abs(top + height - divTop - divHeight)<=0.1){
+                if(imgSrc){
+                    imgsAndNotes.push({alt: imgSrc.split("/").pop(), src: imgSrc, isInlineImg: isInlineImg})
                 }else{
-                    imgsArray.push({name: `${currentContent}-${notesCounter}`, footnote: footnote})
+                    imgsAndNotes.push({name: `${currentContent}-${notesCounter}`, footnote: footnote})
                     notesCounter++
                 }
                 return 1
@@ -41,17 +42,17 @@ function requestImgsArray(imgAndnoteTags,s0,s1,s2){
         height = parseFloat(imgAndnoteTags[i].style.height.replace('px', ''))
         //注释图标的位置需要修正
         if(imgSrc){
+            isInlineImg = imgAndnoteTags[i].className.indexOf('h-pic') > -1
             top = parseFloat(imgAndnoteTags[i].style.top.replace('px', ''))
         }else{
             top = parseFloat(imgAndnoteTags[i].style.top.replace('px', '')) + 2.8
         }
         //遍历各级别标注
-        if(!ergodic(s0, imgSrc))
-            if(!ergodic(s1, imgSrc))
-                ergodic(s2, imgSrc)
+        if(!ergodic(s0))
+            if(!ergodic(s1))
+                ergodic(s2)
     }
-    console.log(imgsArray)
-    return imgsArray
+    return imgsAndNotes
 }
 
 //入口
@@ -61,8 +62,8 @@ function main(){
     var s0 = document.getElementsByClassName("wr_underline s0");
     var s1 = document.getElementsByClassName("wr_underline s1");
     var s2 = document.getElementsByClassName("wr_underline s2");
-    var imgsArray = requestImgsArray(imgAndnoteTags,s0,s1,s2);
-    setMesToBg(imgsArray)
+    var imgsAndNotes = requestImgNotes(imgAndnoteTags,s0,s1,s2);
+    setMesToBg(imgsAndNotes)
 }
 
 //console.log("inject-copyImgs.js：被注入")
