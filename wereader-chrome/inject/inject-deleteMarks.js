44f5cd40e11ddc888ad326dc44c9f2459e034bde
Liuhao <326047736@qq.com>
2021-02-27 14:01:15 +0800

    simplify inject-deleteMarks.js

--- wereader-chrome/inject/inject-deleteMarks.js
+++ wereader-chrome/inject/inject-deleteMarks.js
@@ -10,23 +10,22 @@ Swal.fire({
 }).then((confirm)=>{
         if(!confirm.value) return;
         let underlines = document.querySelectorAll('.wr_underline.s0,.wr_underline.s1,.wr_underline.s2');
-        let counter = 0;
-        underlines.forEach((el)=>{
-            counter++;
+        for (var i = 0; i < underlines.length; i++) {
+            const el = underlines[i];
             setTimeout(() => {
                 el.click();
                 let deleteBtn = document.getElementsByClassName('toolbarItem removeUnderline')[0];
                 if(deleteBtn) deleteBtn.click();
                 // 点击包含想法的标注时会弹出想法框，所以点击空白处使其隐藏
                 if(document.getElementById('readerReviewDetailPanel')) document.elementFromPoint(0, 0).click();
-            }, counter*500);
-        });
+            }, i*500);
+        }
 
         const Toast = Swal.mixin({
             toast: true,
             position: 'top-end',
             showConfirmButton: false,
-            timer: counter*500,
+            timer: i*500,
             timerProgressBar: true
         });
         Toast.fire({
