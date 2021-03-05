cc0ee73aaee5af15a7ee078128b02b2df93f7cd1
Liuhao <326047736@qq.com>
2021-03-05 12:14:43 +0800

    Simplify code

--- wereader-chrome/content/content-markedData.js
+++ wereader-chrome/content/content-markedData.js
@@ -1,37 +1,9 @@
-/*用于获取被标注的图片的Markdown文本数组，用于支持导出被标注的图片*/
-
-class Img{
-    constructor(alt, src, height, top, isInlineImg){
-        this.alt = alt
-        this.src = src
-        this.height = height
-        this.top = top
-        this.isInlineImg = isInlineImg
-    }
-}
-
-class FootNote{
-    constructor(name, footnote, height, top){
-        this.name = name
-        this.height = height
-        this.footnote = footnote
-        this.top = top
-    }
-}
-
-class Pre{
-    constructor(height, top, code){
-        this.height = height
-        this.code = code
-        this.top = top
-    }
-}
-
+/*用于获取被标注的图片的 Markdown 文本数组，用于支持导出被标注的图片*/
 //将所有图片、脚注、代码块放到一个数组中
-function concatElement(imgAndNoteElems, preElems){
-    let objArr = []
-    let currentContent = ''
-    let notesCounter = 0
+function getElObj(){
+    let targetEls = 
+        document.querySelectorAll(".wr_readerImage_opacity,.reader_footer_note.js_readerFooterNote.wr_absolute,pre");
+    let elObjArr = [], currentContent = '', notesCounter = 0;
     //获取当前目录
     if(document.getElementsByClassName("readerTopBar_title_chapter")[0]){
         currentContent = document.getElementsByClassName("readerTopBar_title_chapter")[0].textContent
@@ -39,98 +11,62 @@ function concatElement(imgAndNoteElems, preElems){
         currentContent = document.getElementsByClassName("chapterItem chapterItem_current")[0].childNodes[0].childNodes[0].textContent
     }
     //处理图片和注释
-    for (let i = 0; i < imgAndNoteElems.length; i++) {
-        const element = imgAndNoteElems[i];
-        let imgSrc = element.getAttribute("data-src");
-        let footnote = element.getAttribute("data-wr-footernote");
-        let height = parseFloat(element.style.height.replace('px', ''))
+    targetEls.forEach(el=>{
+        let imgSrc = el.getAttribute("data-src");
+        let footnote = el.getAttribute("data-wr-footernote");
+        let height = parseFloat(el.style.height.replace('px', ''));
+        let top = parseFloat(el.style.top.replace('px', ''));
         if(imgSrc){
             //判断是否为行内图片
-            let isInlineImg = element.className.indexOf('h-pic') > -1;
-            let top = parseFloat(element.style.top.replace('px', ''));
-            objArr.push(new Img(imgSrc.split("/").pop(),imgSrc,height,top,isInlineImg));
+            let isInlineImg = el.className.indexOf('h-pic') > -1;
+            let alt = imgSrc.split("/").pop();
+            elObjArr.push({alt: alt, imgSrc: imgSrc, height: height, top: top, isInlineImg: isInlineImg});
         }else if(footnote){
             //注释图标需要用 2.8 修正
-            let top = parseFloat(element.style.top.replace('px', '')) + 2.8;
-            objArr.push(new FootNote(`${currentContent}-${notesCounter}`,footnote,height,top));
+            top = top + 2.8;
+            let footnoteName = `${currentContent}-${notesCounter}`;
+            elObjArr.push({footnoteName: footnoteName, footnote: footnote, height: height, top: top});
+        }else{//代码块
+            let code = el.textContent;
+            let padding = parseFloat(window.getComputedStyle(el).paddingTop.replace('px', '')) + 
+                parseFloat(window.getComputedStyle(el).paddingBottom.replace('px', ''));
+            height = height + padding;
+            elObjArr.push({height: height, top: top, code: code});
         }
-    }
-    //处理代码块
-    for (let i = 0; i < preElems.length; i++) {
-        const element = preElems[i];
-        let code = element.textContent;
-        let padding = parseFloat(window.getComputedStyle(element).paddingTop.replace('px', '')) + 
-        parseFloat(window.getComputedStyle(element).paddingBottom.replace('px', ''));
-        let height = parseFloat(element.style.height.replace('px', '')) + padding;
-        let top = parseFloat(element.style.top.replace('px', ''));
-        objArr.push(new Pre(height,top,code));
-    }
-
-    objArr.sort(function (x, y) {
-        //需要排序的实际上只有代码块，加 5 可防止同行图片被错误排序
-        let sub = (x.top+x.height) - (y.top+y.height);
-        if (sub<-5) {//y比x明显偏下
-            return -1;
-        }
-        if (sub>5) {//x比y明显偏下
-            return 1;
-        }
-        return 0;
     })
-    return objArr;
+    return elObjArr;
 }
 
 //获取图片和注释
 function getMarkedData(setting){
-    //获取正文所有图片和注释 Element
-    var imgAndNoteTags = document.querySelectorAll(".wr_readerImage_opacity,.reader_footer_note.js_readerFooterNote.wr_absolute");
-    var preElems = document.getElementsByTagName('pre');
-    var objArr = concatElement(imgAndNoteTags, preElems)
+    const elObjArr = getElObj();
     //获取三种标注 Element
-    var s0 = document.getElementsByClassName("wr_underline s0");
-    var s1 = document.getElementsByClassName("wr_underline s1");
-    var s2 = document.getElementsByClassName("wr_underline s2");
-    var wr_myNote = []
-    if(setting.addThoughts){
-        //获取想法标注元素
-        wr_myNote = document.getElementsByClassName("wr_myNote");
-    }
+    let selector = '.wr_underline.s0,.wr_underline.s1,.wr_underline.s2';
+    //获取想法标注 Element
+    if(setting.addThoughts) selector = `${selector},.wr_myNote`;
+    const markMasks = document.querySelectorAll(selector);
     
-    var markedData = []
-    var top = 0, height = 0
-    var isInlineImg = false
-    var imgSrc = '', footnote = '', code = '', footnoteName = ''
-    //根据标注元素与图片/注释在网页中的相对位置来判断图片/注释是否被标注
-    function ergodic(markMask){
-        for(let j=0;j<markMask.length;j++){
-            let divTop = parseFloat(markMask[j].style.top.replace('px', ''))
-            let divHeight = parseFloat(markMask[j].style.height.replace('px', ''))
-            if(Math.abs(top + height - divTop - divHeight)<=0.1){
-                if(imgSrc){
-                    markedData.push({alt: imgSrc.split("/").pop(), src: imgSrc, isInlineImg: isInlineImg})
-                }else if(footnote){
-                    markedData.push({name: footnoteName, footnote: footnote})
-                }else if(code){
-                    markedData.push({code: code})
-                }
-                return 1
-            }
-        }
-        return 0
-    }
+    let markedData = [];
     //遍历 objArr 并逐个检查是否被标注
-    for(let i=0;i<objArr.length;i++){
-        imgSrc = objArr[i].src
-        isInlineImg = objArr[i].isInlineImg
-        footnote = objArr[i].footnote
-        footnoteName = objArr[i].name
-        code = objArr[i].code
-        height = objArr[i].height
-        top = objArr[i].top
-        //遍历各级别标注
-        ergodic(s0) || ergodic(s1) || ergodic(s2) || ergodic(wr_myNote)
-    }
-    return markedData
+    elObjArr.forEach(obj=>{
+        let {imgSrc,alt,isInlineImg,footnote,footnoteName,code,height,top} = obj;
+        //根据标注元素与图片/注释在网页中的相对位置来判断图片/注释是否被标注
+        for (let i = 0; i < markMasks.length; i++) {
+            const mask = markMasks[i];
+            let maskTop = parseFloat(mask.style.top.replace('px', ''));
+            let maskHeight = parseFloat(mask.style.height.replace('px', ''));
+            if(Math.abs(top + height - maskTop - maskHeight)>0.1) continue;
+            if(imgSrc){
+                markedData.push({alt: alt, src: imgSrc, isInlineImg: isInlineImg})
+            }else if(footnote){
+                markedData.push({name: footnoteName, footnote: footnote})
+            }else if(code){
+                markedData.push({code: code})
+            }
+            break;
+        }
+    });
+    return markedData;
 }
 
 // console.log("content-markedData.js：被注入")
@@ -138,6 +74,6 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse)=>{
     if(!request.isGetMarkedData) return;
     chrome.storage.sync.get((setting)=>{
         const markedData = getMarkedData(setting);
-        chrome.runtime.sendMessage({type: 'markedData', markedData: markedData})
+        chrome.runtime.sendMessage({type: 'markedData', markedData: markedData});
     })
 })
