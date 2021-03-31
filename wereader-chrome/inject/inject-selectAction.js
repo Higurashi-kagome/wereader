627627da57a8a7f060991028340c835ed44085af
Liuhao <326047736@qq.com>
2021-03-31 18:19:42 +0800

    Fix multiple calls to the action function

--- wereader-chrome/inject/inject-selectAction.js
+++ wereader-chrome/inject/inject-selectAction.js
@@ -1,74 +1,72 @@
-//标注面板的监听函数
-function onReaderToolbarContainerObserve(mutationsList, observer){
-    for(let mutation of mutationsList) {
-        if (mutation.type === 'attributes' && mutation.attributeName === 'style') {
-            let readerToolbarContainer = document.getElementsByClassName('reader_toolbar_container')[0];
-            //如果选中了文字
-            if(window.getComputedStyle(readerToolbarContainer).display=='block' && document.getElementsByClassName('wr_selection')[0]){
-                //结束监听以方式监听面板消失时触发自动标注
-                observer.disconnect();
-                const storageKey = 'selectAction'
-                chrome.storage.sync.get([storageKey], function(setting){
-                    let targetUnderlineBtn = document.getElementsByClassName(`toolbarItem ${setting[storageKey]}`)[0]
-                    if(setting[storageKey] != "underlinNone" && targetUnderlineBtn){
-                        targetUnderlineBtn.click()
-                    }
-                    //重新监听
-                    addObserverForReaderToolbarContainer();
-                })
-            }
-        }
-    }
-}
-
-//标注面板的父元素的监听函数
-function onRenderTargetContainerObserve(mutationsList, observer){
-    for(let mutation of mutationsList) {
-        if (mutation.type === 'childList') {
-            let readerToolbarContainer = document.getElementsByClassName('reader_toolbar_container')[0];
-            //如果标注面板出现
-            if(readerToolbarContainer){
-                //开始监听标注面板
-                addObserverForReaderToolbarContainer();
-                //在监听到标注面板出现后结束监听
-                observer.disconnect();
-            }
-            //如果选中了文字
-            if(window.getComputedStyle(readerToolbarContainer).display=='block' && document.getElementsByClassName('wr_selection')[0]){
-                const storageKey = 'selectAction'
-                chrome.storage.sync.get([storageKey], function(setting){
-                    let targetUnderlineBtn = document.getElementsByClassName(`toolbarItem ${setting[storageKey]}`)[0]
-                    if(setting[storageKey] != "underlinNone" && targetUnderlineBtn){
-                        targetUnderlineBtn.click();
-                    }
-                })
-            }
-        }
-    }
-}
-
-//为标注面板（readerToolbarContainer）添加监听函数
-function addObserverForReaderToolbarContainer() {
-    let observer = new MutationObserver(onReaderToolbarContainerObserve);
-    let readerToolbarContainer = document.getElementsByClassName('reader_toolbar_container')[0];
-    if(!readerToolbarContainer) {
-        window.setTimeout(addObserverForReaderToolbarContainer,500);
-        return;
-    }
-    observer.observe(readerToolbarContainer, {'attributes':true});
-}
-
-//在不使用标注之前，页面中不存在 readerToolbarContainer，这导致第一次标注时不会触发自动标注，所以使用该函数设置对 readerToolbarContainer 的父元素（renderTargetContainer）的监听
-function addObserverForRenderTargetContainer(){
-    let observer = new MutationObserver(onRenderTargetContainerObserve);
-    let renderTargetContainer = document.getElementsByClassName('renderTargetContainer')[0];
-    if(!renderTargetContainer){
-        window.setTimeout(addObserverForRenderTargetContainer,500);
-        return;
-    }
-    observer.observe(renderTargetContainer, {'childList':true});
-}
-
 // console.log('inject-selectAction.js', '注入');
 // 为处理某些时候切换章节后动作不生效的问题而将该脚本设置为 inject.js（原先为 content.js）
-addObserverForRenderTargetContainer();
+if(!addObserverForRenderTargetContainer){
+    //标注面板的监听函数
+    let onReaderToolbarContainerObserve =  (mutationsList, observer)=>{
+        for(let mutation of mutationsList) {
+            if (mutation.type === 'attributes' && mutation.attributeName === 'style') {
+                let readerToolbarContainer = document.getElementsByClassName('reader_toolbar_container')[0];
+                //如果选中了文字
+                if(window.getComputedStyle(readerToolbarContainer).display=='block' && document.getElementsByClassName('wr_selection')[0]){
+                    //结束监听以方式监听面板消失时触发自动标注
+                    observer.disconnect();
+                    const storageKey = 'selectAction'
+                    chrome.storage.sync.get([storageKey], function(setting){
+                        let targetUnderlineBtn = document.getElementsByClassName(`toolbarItem ${setting[storageKey]}`)[0]
+                        if(setting[storageKey] != "underlinNone" && targetUnderlineBtn){
+                            targetUnderlineBtn.click()
+                        }
+                        //重新监听
+                        addObserverForReaderToolbarContainer();
+                    })
+                }
+            }
+        }
+    }
+    //标注面板的父元素的监听函数
+    let onRenderTargetContainerObserve = (mutationsList, observer)=>{
+        for(let mutation of mutationsList) {
+            if (mutation.type === 'childList') {
+                let readerToolbarContainer = document.getElementsByClassName('reader_toolbar_container')[0];
+                //如果标注面板出现
+                if(readerToolbarContainer){
+                    //开始监听标注面板
+                    addObserverForReaderToolbarContainer();
+                    //在监听到标注面板出现后结束监听
+                    observer.disconnect();
+                }
+                //如果选中了文字
+                if(window.getComputedStyle(readerToolbarContainer).display=='block' && document.getElementsByClassName('wr_selection')[0]){
+                    const storageKey = 'selectAction'
+                    chrome.storage.sync.get([storageKey], function(setting){
+                        let targetUnderlineBtn = document.getElementsByClassName(`toolbarItem ${setting[storageKey]}`)[0]
+                        if(setting[storageKey] != "underlinNone" && targetUnderlineBtn){
+                            targetUnderlineBtn.click();
+                        }
+                    })
+                }
+            }
+        }
+    }
+    //为标注面板（readerToolbarContainer）添加监听函数
+    let addObserverForReaderToolbarContainer = ()=>{
+        let observer = new MutationObserver(onReaderToolbarContainerObserve);
+        let readerToolbarContainer = document.getElementsByClassName('reader_toolbar_container')[0];
+        if(!readerToolbarContainer) {
+            window.setTimeout(addObserverForReaderToolbarContainer,500);
+            return;
+        }
+        observer.observe(readerToolbarContainer, {'attributes':true});
+    }
+    //在不使用标注之前，页面中不存在 readerToolbarContainer，这导致第一次标注时不会触发自动标注，所以使用该函数设置对 readerToolbarContainer 的父元素（renderTargetContainer）的监听
+    var addObserverForRenderTargetContainer =  ()=>{
+        let observer = new MutationObserver(onRenderTargetContainerObserve);
+        let renderTargetContainer = document.getElementsByClassName('renderTargetContainer')[0];
+        if(!renderTargetContainer){
+            window.setTimeout(addObserverForRenderTargetContainer,500);
+            return;
+        }
+        observer.observe(renderTargetContainer, {'childList':true});
+    }
+    addObserverForRenderTargetContainer();
+}
