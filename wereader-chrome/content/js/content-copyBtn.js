8c5bfc72a485ede2ab77ac61d4324f7d94c0bd73
Hao Liu <326047736@qq.com>
2021-06-13 22:22:57 +0800

    fix: wr_underline disturb fancybox/copyBtn

--- wereader-chrome/content/js/content-copyBtn.js
+++ wereader-chrome/content/js/content-copyBtn.js
@@ -135,3 +135,35 @@ chrome.runtime.onMessage.addListener(function(msg){
         }
     });
 });
+
+// 在图片或代码被标注的时候不能够显示复制按钮，所以下面的代码将会监听 Ctrl 键，Ctrl 键按下时鼠标下的标注元素
+// 将被隐藏，此时复制按钮就能够正常使用
+var pressedKeys = {};
+var mousePagePosition = {};
+var mouseClientPosition = {};
+var mouseMoveTarget;
+
+function documentMouseMove(event) {
+	// Store mouse position
+	mousePagePosition = {top:event.pageY, left:event.pageX};
+	mouseClientPosition = {top:event.clientY, left:event.clientX};
+	mouseMoveTarget = event.target;
+}
+
+function documentKeyDown(event) {
+	pressedKeys[event.keyCode] = true;
+	if(pressedKeys[17]) {
+		if(mouseMoveTarget.className.indexOf('wr_underline')>=0){
+			mouseMoveTarget.style.display = 'none';
+		}
+	}
+}
+
+function documentKeyUp(event) {
+	pressedKeys[event.keyCode] = false;
+	if(event.keyCode == 17) {
+		$('.wr_underline').css('display','block');
+	}
+}
+
+$(document).mousemove(documentMouseMove).keydown(documentKeyDown).keyup(documentKeyUp);
