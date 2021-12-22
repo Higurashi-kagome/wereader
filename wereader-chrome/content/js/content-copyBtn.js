15da11cb566ab32344f5374ee590e8be1e92fc48
Hao Liu <326047736@qq.com>
2021-12-22 21:30:03 +0800

    Update content-copyBtn.js
    Resolve #50

--- wereader-chrome/content/js/content-copyBtn.js
+++ wereader-chrome/content/js/content-copyBtn.js
@@ -136,8 +136,8 @@ chrome.runtime.onMessage.addListener(function(msg){
     });
 });
 
-// 在图片或代码被标注的时候不能够显示复制按钮，所以下面的代码将会监听 Ctrl 键，Ctrl 键按下时鼠标下的标注元素
-// 将被隐藏，此时复制按钮就能够正常使用
+/* 在图片或代码被标注的时候不能够显示复制按钮，所以下面的代码将会监听 Ctrl 键，Ctrl 键按下时鼠标下的标注元素
+将被隐藏，此时复制按钮就能够正常使用 */
 var pressedKeys = {};
 var mousePagePosition = {};
 var mouseClientPosition = {};
@@ -166,4 +166,16 @@ function documentKeyUp(event) {
 	}
 }
 
-$(document).mousemove(documentMouseMove).keydown(documentKeyDown).keyup(documentKeyUp);
+// 在按下 Ctrl 标注元素消失后右击鼠标，在松开 Ctrl 时标注将不会恢复显示，所以在此处监听右键
+$(document).mousemove(documentMouseMove).keydown(documentKeyDown).keyup(documentKeyUp).mousedown(
+    function(e) {
+        //右键为 3
+        if (3 == e.which) {
+            setTimeout(() => {
+                $('.wr_underline,.wr_myNote').css('display','block');
+            }, 500);
+        } else if (1 == e.which) {
+            //左键为 1
+        }
+    }
+)
