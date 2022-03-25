9dc1a25d43fd0c0eb75ed5d62e34b9b152c9cb19
Hao Liu <326047736@qq.com>
2022-03-25 22:23:39 +0800

    improve: notesMenu display cur title, simplify

--- wereader-chrome/content/js/content-notesMenu.js
+++ wereader-chrome/content/js/content-notesMenu.js
@@ -1,18 +1,33 @@
 
 document.querySelector('.note').addEventListener('click',function(){
-	if (document.getElementById('sectionListItem_title_scroll')) return;
-	// #noteTools 在 searchNote 中创建
-	let btn = $(`<button
-	style='font-size: 16px;padding: 12px 20px 12px 40px;border:1px groove; border-bottom: none; text-align: left;'>目录</button>`).prependTo('#noteTools');
-	let ul = $(`<ul id='sectionListItem_title_scroll'></ul>`).prependTo('#noteTools');
+	const curChapTitle = window.getCurrentChapTitle();
+	const title_scroll_selector = '#sectionListItem_title_scroll';
+	// 已经创建
+	if ($(title_scroll_selector).length) {
+		// 删除之前章节标注
+		$(`${title_scroll_selector} a.current`).removeClass('current');
+		// 标记当前章节
+		$(`${title_scroll_selector} a`).each((idx, el)=>{
+			let title = $(el);
+			if (title.text() == curChapTitle) title.addClass('current');
+		})
+		return;
+	}
+	// 开始创建
+	let btn = $(`<button class='notesMenuBtn'>目录</button>`).prependTo('#noteTools'); // #noteTools 在 searchNote 中创建
+	let ul = $(`<ul id='${title_scroll_selector.replace("#", "")}'></ul>`).prependTo('#noteTools');
 	$('.sectionListItem_title').each((idx, el)=>{
-		let li = $(`<li><a>${el.innerText}</a></li>`).click(()=>{
+		// 标记当前章节
+		const titleText = el.innerText;
+		const className = (titleText == curChapTitle) ? 'current' : '';
+		// 生成目录
+		let li = $(`<li><a class='${className}'>${titleText}</a></li>`).click(()=>{
 			el.parentElement.scrollIntoView({behavior: "smooth"});
 			ul.toggle();
 		});
 		ul.append(li);
 	});
 	btn.click(()=>{
-		$('#sectionListItem_title_scroll').toggle();
+		$(title_scroll_selector).toggle();
 	});
 },false);
