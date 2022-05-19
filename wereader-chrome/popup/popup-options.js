086d16ca87c385d1215b1ba4e549a8f666dab003
Hao Liu <326047736@qq.com>
2022-05-19 12:56:30 +0800

    clear: remove wereader-chrome

--- wereader-chrome/popup/popup-options.js
+++
@@ -1,6 +0,0 @@
-// 选项页
-if(bg.Config.enableOption){
-	const option = $(`<button class="tabLinks" id="openOption">选项</button>`);
-	$('.tab').append(option);
-	option.click(()=>{ chrome.runtime.openOptionsPage(); });
-}
--- wereader-chrome/popup/popup-vars.js
+++
@@ -1,3 +0,0 @@
-/* 存放全局变量 */
-// 背景页，后面会用到
-const bg = chrome.extension.getBackgroundPage();
