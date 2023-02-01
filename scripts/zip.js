639d591763af9f2834960e6044f3336b90e33c24
Hao Liu <higurashi-kagome@qq.com>
2023-02-01 22:05:37 +0800

    zip single files to the zip file root

--- scripts/zip.js
+++ scripts/zip.js
@@ -1,19 +1,39 @@
 // dist 目录打包为 zip
+const fs = require("fs")
+const path = require("path")
+const chalk = require("chalk")
+const archiver = require("archiver")
 
-const path = require("path");
-const chalk = require("chalk");
-const compressing = require("compressing");
-
-const resolve = dir => path.join(__dirname, "..", dir);
+const resolve = file => path.join(__dirname, "..", file)
 const zipName = 'wereader.zip'
+const zipPath = resolve(zipName)
 
-const zipPath = resolve(zipName);
-compressing.zip
-  .compressDir(resolve("dist/"), zipPath)
-  .then(() => {
-    console.log(chalk.yellow(`Tip: 文件压缩成功，已压缩至【${zipPath}】`));
-  })
-  .catch(err => {
-    console.log(chalk.red("Tip: 压缩报错"));
-    console.error(err);
-  });
+function walkSync(currentDirPath, callback) {
+	fs.readdirSync(currentDirPath, { withFileTypes: true }).forEach(function(dirent) {
+		const filePath = path.join(currentDirPath, dirent.name)
+		if (dirent.isFile()) {
+			callback(filePath, dirent)
+		} else if (dirent.isDirectory()) {
+			walkSync(filePath, callback)
+		}
+	})
+}
+
+fs.unlinkSync(zipPath)
+
+const paths = []
+walkSync('dist', function(filepath, stat) {
+	paths.push({filename: filepath.replace(/^dist/, ""), filepath})
+})
+const inputSources = paths.map(p => ({
+    readStream: fs.createReadStream(p.filepath),
+    name: p.filename
+}))
+const outputStream = fs.createWriteStream(zipPath)
+const archive = archiver('zip', {
+    zlib: { level: 9 }
+})
+archive.pipe(outputStream)
+inputSources.forEach(src => archive.append(src.readStream, { name: src.name }))
+archive.finalize()
+console.log(chalk.yellow(`打包成功：${zipPath}`))
