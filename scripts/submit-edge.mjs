ced7e017a9c4d84e8ab43646faf438333257eb9d
Hao Liu <higurashi-kagome@qq.com>
2023-02-02 20:56:15 +0800

    update deploy script

--- scripts/submit-edge.mjs
+++ scripts/submit-edge.mjs
@@ -1,10 +1,20 @@
 import { EdgeWebstoreClient } from "@plasmo-corp/ewu";
+import * as fs from "fs";
 
 const args = process.argv.slice(2)
+if(args.length < 5){
+	console.log("参数缺失")
+	return
+}
 const productId = args[0]
 const clientId = args[1]
 const clientSecret = args[2]
 const accessTokenUrl = args[3]
+const packFile = args[4]
+if(!fs.existsSync(packFile)){
+	console.log(packFile + "不存在")
+	return
+}
 const client = new EdgeWebstoreClient({
   productId,
   clientId,
@@ -12,7 +22,7 @@ const client = new EdgeWebstoreClient({
   accessTokenUrl
 })
 client.submit({
-	filePath: "./dist.zip"
+	filePath: packFile
 }).then(res=>{
 	console.log(res)
 })
